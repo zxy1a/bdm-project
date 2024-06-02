313241a9759ec2b5f635e8c5f2c3bfd1f2f77928
Xinyue <xinyuezhangcn@gmail.com>
2024-06-02 15:51:54 +0300

    title cleaned

--- clean.py
+++ clean.py
@@ -1,11 +1,11 @@
 # 导入必要的库
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lower, regexp_replace
+from pyspark.sql.functions import col, lower, regexp_replace, concat_ws
 from pyspark.ml.feature import RegexTokenizer, StopWordsRemover
 
 # 创建 SparkSession 并配置资源
 spark = SparkSession.builder \
-    .appName("Text Cleaning") \
+    .appName("Text Cleaning and Clustering") \
     .config("spark.executor.memory", "4g") \
     .config("spark.executor.cores", "4") \
     .config("spark.driver.memory", "4g") \
@@ -16,11 +16,16 @@ data_path = "combined_data_en.json"
 df = spark.read.json(data_path)
 
 # 转换为小写并移除标点符号
+df = df.withColumn("cleaned_title", lower(col("title")))
+df = df.withColumn("cleaned_title", regexp_replace(col("cleaned_title"), r'[!()\-\[\]{};:\'",<>./?@#$%^&*_~]', ''))
 df = df.withColumn("cleaned_abstract", lower(col("abstract")))
 df = df.withColumn("cleaned_abstract", regexp_replace(col("cleaned_abstract"), r'[!()\-\[\]{};:\'",<>./?@#$%^&*_~]', ''))
 
+# 合并标题和摘要
+df = df.withColumn("combined_text", concat_ws(" ", col("cleaned_title"), col("cleaned_abstract")))
+
 # Tokenization
-regex_tokenizer = RegexTokenizer(inputCol="cleaned_abstract", outputCol="words", pattern="\\W")
+regex_tokenizer = RegexTokenizer(inputCol="combined_text", outputCol="words", pattern="\\W")
 df = regex_tokenizer.transform(df)
 
 # 自定义停用词列表
